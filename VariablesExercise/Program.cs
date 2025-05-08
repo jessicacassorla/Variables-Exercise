c858de88661b094e905ecad02db8bfff1341b773
Jessica Cassorla <jessicacassorla@gmail.com>
2025-05-07 21:20:09 -0500

    added new variables about TrueCoders class

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string courseName = "TrueCoders";
+            int courseLength = 5;
+            char courseGrade = 'A';
+            bool isCourseFun = true;
+            double totalClassAverage= 3.5;
+            decimal courseCost = 6500.00m;
+            
+            Console.WriteLine($"Course Name: {courseName}\nWeeks In Class So Far: {courseLength}\nCurrent Grade: {courseGrade}\nCohort GPA: {totalClassAverage}\nCost: ${courseCost}\nIs the Class Fun?: {isCourseFun}");
+            
         }
     }
 }
